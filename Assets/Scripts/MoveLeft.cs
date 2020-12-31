4c8ab1628556033c83aa42834a01cac752df14db
Jehdi Aizon <jaizon2000@gmail.com>
2020-12-30 17:35:48 -0700

    Destroy obstacles that exit bounds

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -7,6 +7,7 @@ public class MoveLeft : MonoBehaviour
     private float speed = 30;
     private PlayerController playerControllerScript;
 
+    private float leftBound = -10;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +18,16 @@ public class MoveLeft : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // If it's game over
         if (playerControllerScript.gameOver == false)
         {
             transform.Translate(Vector3.left * (Time.deltaTime * speed));
+        }
+
+        // if obstacle position x < leftBound
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
         }
     }
 }
