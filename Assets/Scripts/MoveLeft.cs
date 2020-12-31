a6d683f75b401b47db461c59be944dc46d52db0c
Jehdi Aizon <jaizon2000@gmail.com>
2020-12-30 17:29:02 -0700

    Stop MoveLeft on gameOver

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 30;
+    private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Get player game object in PlayerController.cs that has the tag "Player"
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * (Time.deltaTime * speed));
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * (Time.deltaTime * speed));
+        }
     }
 }
