3ba86c6724b61e0af39c612772b9ea570ed84e7a
Jehdi Aizon <jaizon2000@gmail.com>
2021-01-01 20:33:09 -0700

    clean up code

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -10,12 +10,14 @@ public class SpawnManager : MonoBehaviour
     private float startDelay = 2;
     private float repeatRate = 2;
 
+
     private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
         spawnPos = transform.position;
+        
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
